b291a2bce7ea201cbcdd7a42fb7e608cae31a380
FunckyZombie <splinter.hertz@gmail.com>
2022-09-12 02:29:02 +0700

    JS2 Урок 7

---
+++ public/js/ErrorComp.js
@@ -0,0 +1,25 @@
+Vue.component('error', {
+    data(){
+        return {
+          text: ''
+        }
+    },
+    computed: {
+      isVisible(){
+          return this.text !== ''
+      }
+    },
+    methods: {
+      setText(value) {
+          this.text = value;
+      }
+    },
+    template: `
+    <div class="error-block" v-if="isVisible">
+        <p class="error-msg">
+        <button class="close-btn" @click="text=''">&times;</button>
+        {{ text }}
+    </p>
+    </div>
+        `
+})
